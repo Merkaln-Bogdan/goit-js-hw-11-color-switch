1d6b60bf26cd81d4a9e2ec602d284f12b04723d0
IlkaiGundogun <piccasoul19@ukr.net>
2020-04-13 20:55:27 +0300

    first

---
+++ index.js
@@ -0,0 +1,20 @@
+const btnStart = document.querySelector('[data-action="start"]');
+const btnStop = document.querySelector('[data-action="stop"]');
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+let timer1 = null;
+btnStart.addEventListener('click', startButtonClick);
+btnStop.addEventListener('click', stopRandomInteger);
+// Start backgroundColor change
+function startButtonClick() {
+  randomIntegerFromInterval(0, 5);
+  timer1 = setInterval(() => {
+    document.body.style.backgroundColor =
+      colors[randomIntegerFromInterval(0, 5)];
+  }, 3000);
+}
+// Stop backgroundColor change
+function stopRandomInteger() {
+  clearInterval(timer1);
+}
