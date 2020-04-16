171d2e7f34c5cc08eaca28d2d6d10b4ff1916335
IlkaiGundogun <piccasoul19@ukr.net>
2020-04-16 20:13:00 +0300

    Second attempt

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const btnStop = document.querySelector('[data-action="stop"]');
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
+const delay = 3000;
 let timer1 = null;
 btnStart.addEventListener('click', startButtonClick);
 btnStop.addEventListener('click', stopRandomInteger);
 // Start backgroundColor change
 function startButtonClick() {
-  randomIntegerFromInterval(0, 5);
+  randomIntegerFromInterval(0, colors.length);
   timer1 = setInterval(() => {
     document.body.style.backgroundColor =
-      colors[randomIntegerFromInterval(0, 5)];
-  }, 3000);
+      colors[randomIntegerFromInterval(0, colors.length)];
+  }, delay);
 }
 // Stop backgroundColor change
-function stopRandomInteger() {
-  clearInterval(timer1);
+function stopRandomInteger(e) {
+  e.currentTarget = clearInterval(timer1);
 }
 const colors = [
   '#FFFFFF',
